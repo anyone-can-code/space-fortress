977b3d599cdf664ac2dc32d516cff5fee30e2c58
Dmitri Volkov <dmitri.o.volkov@gmail.com>
2020-11-06 15:33:16 -0500

    Add basic world rendering
    Now the world can be seen! It's like magic or something!
    
    Things to improve:
    - connect View to Engine
    - have View iterate when there's multiple things in the same spot,
      ignoring floor

--- src/core/View.java
+++ src/core/View.java
@@ -12,11 +12,18 @@ import javax.swing.JPanel;
 public class View extends JPanel {
 
 	// 3d array where first 2 ds are row and column, 3ed d is array of all Sprites in a location
+	// 64x64x64
 	private Sprite[][][] world;
 
 	public View() {
 		// should initialize a test world here
-		setBackground(Color.RED);
+		world = new Sprite[64][64][64];
+		for (int r = 0; r < 64; r++) {
+			for (int c = 0; c < 64; c++) {
+				world[r][c][0] = new Sprite("#", Color.WHITE, new Point(c, r));
+			}
+		}
+		world[24][31][0] = new Sprite("@", Color.YELLOW, new Point(31, 24));
 	}
 
 	public void update(Sprite[][][] updatedWorld) {
@@ -30,8 +37,24 @@ public class View extends JPanel {
 
 		g.setBackground(Color.BLACK);
 		g.clearRect(0, 0, getWidth(), getHeight());
-
-		g.drawRect(10, 10, 30, 30);
+		
+		// widest character plus padding
+		int spriteBoxSize = g.getFontMetrics().stringWidth("M");
+		
+		// iterate through the three dimensions of world
+		for (int r = 0; r < 64; r++) {
+			for (int c = 0; c < 64; c++) {
+				for (int d = 0; d < 64; d++) {
+					
+					Sprite spr = world[r][c][d];
+					
+					if (!(spr == null)) {
+						g.setColor(spr.getColor());
+						g.drawString(spr.getSymbol(), 20 + (spriteBoxSize * c), 20 + (spriteBoxSize * r));
+					}
+				}
+			}
+		}
 
 	}
 	
